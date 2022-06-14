1c6103747412c313bf89a7dd1b5bf742cbe675e7
c483481 <73603369+c483481@users.noreply.github.com>
2022-06-15 04:10:36 +0800

    Update TimPengembang.php

--- app/views/Perkenalan/TimPengembang.php
+++ app/views/Perkenalan/TimPengembang.php
@@ -5,16 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/perkenalan.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>css/perkenalan.css?v=<?=time(); ?>">
     <title>Nama Pembuat</title>
 </head>
 
 <body>
+    <div> sesuatu </div>
     <div class="teks6">
         <p>Nama Nama Pembuat:</p>
-        <script src="../js/scriptNama.js"></script>
+        <p class="text">1. Charles William Kojansow <br> 20021106051</p>
+        <p class="text">2. Rhesa Rahel Kasakeyan <br> 20021106065</p>
+        <p class="text">3. Niyll Jhonnatan Henry Lewu <br> 20021106151</p>
+        <p class="text">4. Prayer Rafael Novendra <br> 20021106152</p>
         <a href="https://drive.google.com/file/d/1rii8oFjbZWSAkPurLZtLbwHE-lhcQP95/view?usp=sharing">Link Video Presentasi:</a>
-
         <a href="<?= BASEURL ?>Umum">Kembali</a>
     </div>
 </body>
